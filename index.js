e89ad56043cd438b81f550bf6185f292b8b2f0f1
darklegion <Shashank>
2022-05-30 11:16:08 +0530

    final with changes

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-// const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -79,53 +78,6 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
-// app.use(helmet());
-
-
-// const scriptSrcUrls = [
-//   "https://stackpath.bootstrapcdn.com/",
-//   "https://api.tiles.mapbox.com/",
-//   "https://api.mapbox.com/",
-//   "https://kit.fontawesome.com/",
-//   "https://cdnjs.cloudflare.com/",
-//   "https://cdn.jsdelivr.net",
-// ];
-// const styleSrcUrls = [
-//   "https://kit-free.fontawesome.com/",
-//   "https://stackpath.bootstrapcdn.com/",
-//   "https://api.mapbox.com/",
-//   "https://api.tiles.mapbox.com/",
-//   "https://fonts.googleapis.com/",
-//   "https://use.fontawesome.com/",
-// ];
-// const connectSrcUrls = [
-//   "https://api.mapbox.com/",
-//   "https://a.tiles.mapbox.com/",
-//   "https://b.tiles.mapbox.com/",
-//   "https://events.mapbox.com/",
-// ];
-// const fontSrcUrls = [];
-// app.use(
-//   helmet.contentSecurityPolicy({
-//       directives: {
-//           defaultSrc: [],
-//           connectSrc: ["'self'", ...connectSrcUrls],
-//           scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-//           styleSrc: [...styleSrcUrls],
-//           workerSrc: ["'self'", "blob:"],
-//           objectSrc: [],
-//           imgSrc: [
-//               "'self'",
-//               "blob:",
-//               "data:",
-//               "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-//               "https://images.unsplash.com/",
-//           ],
-//           fontSrc: ["'self'", ...fontSrcUrls],
-//       },
-//   })
-// );
-
 
 app.use(passport.initialize());
 app.use(passport.session());
